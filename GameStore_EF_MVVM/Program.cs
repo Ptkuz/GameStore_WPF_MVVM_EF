52bb98a2645b509c0fe3cea0fc48553f55ed99ab
Gurrex <ptkuz@yandex.ru>
2022-06-12 16:04:26 +0700

    Контейнер сервисов

---
+++ GameStore_EF_MVVM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore_EF_MVVM
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args) 
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
