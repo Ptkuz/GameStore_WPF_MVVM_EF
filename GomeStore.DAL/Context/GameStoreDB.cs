43b38421499bc26b2b07641b9293233a52d7234a
Gurrex <ptkuz@yandex.ru>
2022-06-12 17:23:07 +0700

    Создана структура БД

---
+++ GomeStore.DAL/Context/GameStoreDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GomeStore.DAL.Context
+{
+    internal class GameStoreDB
+    {
+    }
+}
---
+++ GomeStore.DAL/Entityes/Deal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GomeStore.DAL.Entityes.Base;
+
+namespace GomeStore.DAL.Entityes
+{
+    public class Deal : Entity
+    {
+        public virtual Game Game { get; set; } = null!;
+        public virtual Seller Seller { get; set; } = null!;
+        public virtual Buyer Buyer { get; set; } = null!;
+    }
+}
