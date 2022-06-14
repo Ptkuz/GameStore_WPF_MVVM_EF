625263c732bc36ba2af97e07b0220e631d12a959
Gurrex <ptkuz@yandex.ru>
2022-06-14 13:27:04 +0700

    Создание инициализатора

--- GameStore_EF_MVVM/Data/DbInitializer.cs
+++ GameStore_EF_MVVM/Data/DbInitializer.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using GameStore_EF_MVVM.Service;
+using System.Threading;
+using System.Diagnostics;
 
 namespace GameStore_EF_MVVM.Data
 {
@@ -13,19 +16,38 @@ namespace GameStore_EF_MVVM.Data
         private readonly GameStoreDB db;
         private readonly ILogger<DbInitializer> logger;
 
-        public DbInitializer(GameStoreDB Db, ILogger<DbInitializer> Logger)
+        public DbInitializer(GameStoreDB Db, ILogger<DbInitializer> logger)
         {
             this.db = Db;
-            logger = Logger;
+            this.logger = logger;
         }
 
         public async Task InitializeAsync()
         {
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация БД...");
+
+            logger.LogInformation("Удаление существующей БД...");
             await db.Database.EnsureDeletedAsync().ConfigureAwait(false);
+            logger.LogInformation($"Удаление выполнено за {0} мс", timer.ElapsedMilliseconds);
+
+
+            logger.LogInformation($"Миграция БД...");
             await db.Database.MigrateAsync().ConfigureAwait(false);
+            logger.LogInformation($"Миграция выполнена за {0} мс", timer.ElapsedMilliseconds);
 
             if (await db.Games.AnyAsync())
                 return;
+
+            await InitializeCategoriesAsync();
+            await InitializePublichersAsync();
+            await InitializeDevelopersAsync();
+            await InitializeGamesAsync();
+            await InitializeBuyersAsync();
+            await InitializeSellersAsync();
+            await InitializeDealsAsync();
+
+            logger.LogInformation($"Инициализация выполнена за {0} с", timer.Elapsed.TotalSeconds);
 
         }
 
@@ -34,7 +56,8 @@ namespace GameStore_EF_MVVM.Data
 
         private async Task InitializeCategoriesAsync()
         {
-
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация категорий");
             Categories = new Category[categoryCount];
             for (int i = 0; i < categoryCount; i++) 
             {
@@ -42,6 +65,49 @@ namespace GameStore_EF_MVVM.Data
             }
             await db.Categories.AddRangeAsync(Categories);
             await db.SaveChangesAsync();
+            logger.LogInformation($"Инициализация категорий выполнена за {0} с", timer.Elapsed.TotalSeconds);
+        }
+
+        private const int publishersCount = 10;
+        private Publicher[] Publichers;
+
+        private async Task InitializePublichersAsync()
+        {
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация издателей");
+            Publichers = new Publicher[publishersCount];
+            for (int i = 0; i < publishersCount; i++)
+            {
+                Publichers[i] = new Publicher { Name = $"Издатель {i + 1}" };
+            }
+            await db.Publichers.AddRangeAsync(Publichers);
+            await db.SaveChangesAsync();
+            logger.LogInformation($"Инициализация издателей выполнена за {0} с", timer.Elapsed.TotalSeconds);
+        }
+
+
+        private const int developersCount = 10;
+        private Developer[] Developers;
+
+        private async Task InitializeDevelopersAsync()
+        {
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация разработчиков");
+            var rnd = new Random();
+
+            Developers = new Developer[developersCount];
+            for (int i = 0; i < developersCount; i++)
+            {
+                Developers[i] = new Developer 
+                { 
+                    Name = $"Разработчик {i + 1}",
+                    Publicher = rnd.NextItem(Publichers)
+                };
+                
+            }
+            await db.Developers.AddRangeAsync(Developers);
+            await db.SaveChangesAsync();
+            logger.LogInformation($"Инициализация разработчиков выполнена за {0} с", timer.Elapsed.TotalSeconds);
         }
 
 
@@ -51,11 +117,14 @@ namespace GameStore_EF_MVVM.Data
 
         private async Task InitializeGamesAsync()
         {
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация игр");
             var rnd = new Random();
 
             Games = Enumerable.Range(1, gamesCount)
                 .Select(i=> new Game 
                 { 
+                    ReleaseDate = Extentions.RandomDate(rnd),
                     Name = $"Игра {i}",
                     Category = rnd.NextItem(Categories)
 
@@ -64,6 +133,90 @@ namespace GameStore_EF_MVVM.Data
 
             await db.Games.AddRangeAsync(Games);
             await db.SaveChangesAsync();
+            logger.LogInformation($"Инициализация игр выполнена за {0} с", timer.Elapsed.TotalSeconds);
+
+        }
+
+        private const int sellersCount = 10;
+        private Seller[] Sellers;
+
+        private async Task InitializeSellersAsync()
+        {
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация продавцов");
+            var rnd = new Random();
+
+            Sellers = new Seller[sellersCount];
+            for (int i = 0; i < sellersCount; i++)
+            {
+                Sellers[i] = new Seller
+                {
+                    Name = $"Продавец Имя {i + 1}",
+                    Surname = $"Продавец Фамилия {i+1}",
+                    Patronymic = $"Продавец Отчество {i+1}"
+
+                    
+                };
+
+            }
+            await db.Sellers.AddRangeAsync(Sellers);
+            await db.SaveChangesAsync();
+            logger.LogInformation($"Инициализация продавцов выполнена за {0} с", timer.Elapsed.TotalSeconds);
+        }
+
+
+        private const int buyersCount = 10;
+        private Buyer[] Buyers;
+
+        private async Task InitializeBuyersAsync()
+        {
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация покупателей");
+            var rnd = new Random();
+
+            Buyers = new Buyer[buyersCount];
+            for (int i = 0; i < buyersCount; i++)
+            {
+                Buyers[i] = new Buyer
+                {
+                    Name = $"Покупатель Имя {i + 1}",
+                    Surname = $"Покупатель Фамилия {i + 1}",
+                    Patronymic = $"Покупатель Отчество {i + 1}"
+
+
+                };
+
+            }
+            await db.Sellers.AddRangeAsync(Sellers);
+            await db.SaveChangesAsync();
+            logger.LogInformation($"Инициализация покупателей выполнена за {0} с", timer.Elapsed.TotalSeconds);
+        }
+
+
+        private const int dealsCount = 1000;
+        private Deal[] Deals;
+        private async Task InitializeDealsAsync()
+        {
+            var timer = Stopwatch.StartNew();
+            logger.LogInformation("Инициализация сделок");
+            var rnd = new Random();
+
+            for (int i = 0; i<dealsCount; i++) 
+            {
+                Deals[i] = new Deal 
+                {
+                    Price = (decimal)rnd.NextDouble(100, 2000),
+                    DateTime = DateTime.Now,
+                    Game = rnd.NextItem(Games),
+                    Seller = rnd.NextItem(Sellers),
+                    Buyer = rnd.NextItem(Buyers)
+                };
+                //Thread.Sleep(1000);
+            }
+
+            await db.Deals.AddRangeAsync(Deals);
+            await db.SaveChangesAsync();
+            logger.LogInformation($"Инициализация сделок выполнена за {0} с", timer.Elapsed.TotalSeconds);
 
         }
     }
---
+++ GameStore_EF_MVVM/Service/Extentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore_EF_MVVM.Service
+{
+    
+        public static class Extentions
+        {
+            public static DateTime RandomDate(Random random)
+            {
+                DateTime start = new DateTime(2000, 1, 1);
+                int range = (DateTime.Today - start).Days;
+                return start.AddDays(random.Next(range));
+            }
+        }
+    
+}
