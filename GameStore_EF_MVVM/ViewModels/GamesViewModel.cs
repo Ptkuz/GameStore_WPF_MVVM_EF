f58ee828f715725995d13ad03fa82d0c4e7fe0ee
Gurrex <ptkuz@yandex.ru>
2022-06-18 15:26:35 +0700

    Создан фильтр

--- GameStore_EF_MVVM/ViewModels/GamesViewModel.cs
+++ GameStore_EF_MVVM/ViewModels/GamesViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using GameStore.DAL.Entityes;
 using GameStore.Interfaces;
 using MathCore.WPF.ViewModels;
@@ -12,16 +14,55 @@ namespace GameStore_EF_MVVM.ViewModels
     internal class GamesViewModel : ViewModel
     {
         private readonly IRepository<Game> gamesRepository;
+        private CollectionViewSource GamesViewSource;
 
         public IEnumerable<Game> Games => gamesRepository.Items;
 
-        public string TestValue { get; } = "Test Value!!!";
+        #region Фильтруищее выражение
+        private string gamesFilter;
+        public string GamesFilter 
+        { 
+            get 
+            { 
+                return gamesFilter; 
+            }
+            set 
+            {
+                if (Set(ref gamesFilter, value)) 
+                    GamesViewSource.View.Refresh();
+            }
+        }
+        #endregion
 
-        
+
+        public ICollectionView GamesView { get { return GamesViewSource.View; } }
 
         public GamesViewModel(IRepository<Game> gamesRepository)
         {
             this.gamesRepository = gamesRepository;
+            GamesViewSource = new CollectionViewSource()
+            {
+                Source = gamesRepository.Items.ToArray(),
+                SortDescriptions =
+                {
+                    new SortDescription(nameof(Game.Name), ListSortDirection.Ascending)
+                }
+                
+            
+            };
+            GamesViewSource.Filter += OnGamesFilter;
+        }
+
+        private void OnGamesFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Game game) || string.IsNullOrEmpty(GamesFilter)) return;
+
+            if (!game.Name.Contains(GamesFilter) &&
+                !game.Developer.Name.Contains(GamesFilter) &&
+                !game.ReleaseDate.ToString().Contains(GamesFilter)) 
+            {
+                e.Accepted = false;
+            }
         }
     }
 }
